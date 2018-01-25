af57d67fc29e94259df93bbea8ff0299dd5f38be
Taemin Kim <babotaemin@gmail.com>
2018-01-25 16:56:16 +0900

    오타수정

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -3,7 +3,7 @@
       <v-flex xs6 offset-xs3>
           <div class="white elevation-2">
               <v-toolbar flat dense class="cyan" dark>
-                <v-tollbar-title>Register</v-tollbar-title>
+                <v-toolbar-title>Register</v-toolbar-title>
               </v-toolbar>
               <div class="pl-4 pr-4 pt-2 pb-2">
                     <input
